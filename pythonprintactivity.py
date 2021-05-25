00bc9aa808b5ddf3dbc523d58ada063b76f9c770
Garnace0923 <viel.garnace@gmail.com>
2021-05-25 21:57:52 +0800

    V 1.3 Garnace Viel

---
+++ pythonprintactivity.py
@@ -0,0 +1,39 @@
+nameuser = input("Enter Fullname: ")
+coursec = input("Enter Course and Section: ")
+contactnum = input("Enter Phone/Contact Number: ")
+emailadd = input("Enter Email Address: ")
+
+
+print("Name: "+nameuser)
+print("Course and Section: "+coursec)
+print("Contact Number: "+contactnum)
+print("Email Adress: "+emailadd)
+
+
+print("____ ____ ____")
+print("| X |  X | O  | ")
+print("____ ____ ____")
+print("| O |  O |    | ")
+print("____ ____ ____")
+print("| X |    |    | ")
+print("____ ____ ____")
+
+
+print("              ")
+
+
+print("M    M  FFFFFF")
+print("MM  MM  FF    ")
+print("M MM M  FFFFFF")
+print("M    M  FF    ")
+print("M    M  FF    ")
+
+
+print("  ------------   ")
+print(" |  ___  ___  |  ")
+print(" |  | |  | |  |  ")
+print(" |  | |  | |  |  ")
+print(" |            |  ")
+print("  | \      / |  ")
+print("   | \____/ |  ")
+print("    --------  ")
